cf2f026311b1ed0a84d8d28c131e7f90f8368012
Ritesh Shah <arits.king74@gmail.com>
2023-10-28 18:25:34 -0500

    Learned Traversals in Binary Tree

--- src/Node.java
+++ src/Node.java
@@ -1,9 +1,10 @@
 public class Node {
     int data;
-    Node left, right;
+    Node left;
+    Node right;
 
-    public Node(int num) {
-        this.data = num;
+    public Node(int data) {
+        this.data = data;
 
     }
 
@@ -14,29 +15,61 @@ public class Node {
             } else {
                 left.insert(num);
             }
-        }else{
-            if(right ==null){
-                right=new Node(num);
-            }else{
+        } else {
+            if (right == null) {
+                right = new Node(num);
+            } else {
                 right.insert(num);
             }
         }
     }
-    public boolean contains(int num){
-        if(data==num){  //if the number i am looking for matched the value at the node
+
+    public boolean contains(int num) {
+        if (data == num) {  //if the number i am looking for matched the value at the node
             return true;
-        }else if (num<data){ //if the number i am looking for is less than node it's going to be one left
-            if(left==null){  //if left node is empty then we definitely don't have the num
+        } else if (num < data) { //if the number i am looking for is less than node it's going to be one left
+            if (left == null) {  //if left node is empty then we definitely don't have the num
                 return false;
-            }else{
+            } else {
                 return left.contains(num); //else we recursively call left.node and check one by one until one of the condition is satisfied.
             }
-        }else{
-            if(right==null){
+        } else {
+            if (right == null) {
                 return false;
-            }else{
+            } else {
                 return right.contains(num);
             }
         }
     }
+
+    public void inOrderTraversing() {  // left --> node--> right
+        if (left != null) {
+            left.inOrderTraversing();
+        }
+        System.out.print(data + " ");
+        if (right != null) {
+            right.inOrderTraversing();
+        }
+    }
+
+    public void postOrderTraversing() {  // left--> right --> node
+        if (left != null) {
+            left.postOrderTraversing();
+        }
+        if (right != null) {
+            right.postOrderTraversing();
+        }
+        System.out.print(data + " ");
+    }
+
+    public void preOrderTraversing() {    // node--> left --> right
+        System.out.print(data + " ");
+        if (left != null) {
+            left.preOrderTraversing();
+        }
+        if (right != null) {
+            right.preOrderTraversing();
+        }
+    }
+
 }
--- src/TreeApplicationClass.java
+++ src/TreeApplicationClass.java
@@ -1,11 +1,22 @@
 public class TreeApplicationClass {
     public static void main(String[] args) {
-        Node node= new Node(0);
+        Node node= new Node(2);
         node.insert(1);
-        node.insert(2);
-        node.insert(3);
+        node.insert(10);
         node.insert(4);
+        node.insert(3);
         node.insert(5);
         node.insert(6);
+        System.out.println(node.contains(1));
+       System.out.println(node.contains(100));
+        System.out.print("In Order Traversing: ");
+        node.inOrderTraversing();
+        System.out.println(" ");
+        System.out.print("Pre Order Traversing: ");
+        node.preOrderTraversing();
+        System.out.println(" ");
+        System.out.print("Post Order Traversing: ");
+        node.postOrderTraversing();
+
     }
 }
